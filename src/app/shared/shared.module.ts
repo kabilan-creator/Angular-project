949656dd2ceb9bf2ad7a01b34b571f593b16b188
kabilan <kabilan172001@gmail.com>
2025-03-06 12:05:40 +0530

    Initial commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { CardComponent } from "./card/card.component";
-import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations:[CardComponent],
